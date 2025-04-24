f684fc417032490e6f071bd38d7405bb49379334
Shvetkoy Anton <220675@astanait.edu.kz>
2025-04-24 17:07:43 +0500

    State class changed
    Added loading from SaveSystem, limited the maximum and minimum values ​​of states. For State variables: 0.0 means a negative state, 1.0 means a positive state.

--- elements/pet/state.gd
+++ elements/pet/state.gd
@@ -1,27 +1,37 @@
 class_name State
 extends Node
 
-var care : float = 4
-var happiness : float = 3
-var hunger : float = 3
-var cleanliness : float = 3
+var care : float
+
+# State variables
+var happiness : float
+var hunger : float
+var cleanliness : float
+
+
+func _ready() -> void:
+	# TODO: Make calculations of state values ​​depending on the last visit to the game
+	care = SaveSystem.get_var("care", 4.0)
+	happiness = SaveSystem.get_var("happiness", 1.0)
+	hunger = SaveSystem.get_var("hunger", 1.0)
+	cleanliness = SaveSystem.get_var("cleanliness", 1.0)
 
 
 func change_care(amount : float) -> void:
-	care += amount
+	care = clamp(care + amount, 0.0, 7.0)
 	Events.care_changed.emit(care)
 
 
 func change_happy(amount : float) -> void:
-	happiness += amount
+	happiness = clamp(happiness + amount, 0.0, 1.0)
 	Events.happiness_changed.emit(happiness)
 
 
 func change_hunger(amount) -> void:
-	hunger += amount
+	hunger = clamp(hunger + amount, 0.0, 1.0)
 	Events.hunger_changed.emit(hunger)
 
 
 func change_clean(amount) -> void:
-	cleanliness += amount
+	cleanliness = clamp(cleanliness + amount, 0.0, 1.0)
 	Events.cleanliness_changed.emit(cleanliness)
