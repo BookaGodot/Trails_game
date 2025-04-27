202d58e842a75e1baf91310f815d5172e627ff35
PettyGlare <128653181+PettyGlare@users.noreply.github.com>
2025-04-27 22:27:44 +0500

    merge attempt 1

--- elements/pet/pet.gd
+++ elements/pet/pet.gd
@@ -7,3 +7,33 @@ enum Temperaments { NEUTRAL, FUNNY, ANXIOUS, SHY, MELANCHOLY, STUBBORN }
 
 var age : float = 0.0
 var temperament : Temperaments = Temperaments.NEUTRAL
+
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventKey and event.is_pressed():
+		if event.keycode == KEY_F:
+			feed()
+		elif event.keycode == KEY_C:
+			clean()
+		elif event.keycode == KEY_H:
+			make_happy()
+
+
+func feed() -> void:
+	state.change_hunger(1.0)
+	# TODO: add animation
+
+
+func clean() -> void:
+	state.change_cleanliness(1.0)
+	# TODO: add animation
+
+
+func make_happy() -> void:
+	state.change_happiness(1.0)
+	# TODO: add animation
+
+
+func _on_click() -> void:
+	print("Pet clicked")
+	# TODO: add animation
